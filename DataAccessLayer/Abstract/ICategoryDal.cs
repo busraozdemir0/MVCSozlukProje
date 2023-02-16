f3124cbc0fbbdcd5298bdfe378626240b88b0dc2
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-02-16 14:02:35 +0300

    15. ders sonu

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD metodları
+        List<Category> List();
+        void Insert(Category c);
+        void Update(Category c);    
+        void Delete(Category c);    
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category c)
+        {
+            _object.Remove(c);
+            context.SaveChanges();
+        }
+
+        public void Insert(Category c)
+        {
+            _object.Add(c);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category c)
+        {
+            context.SaveChanges();
+        }
+    }
+}
