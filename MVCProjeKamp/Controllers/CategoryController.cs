cdd398a5ae401d6eb6e8048a4c46ab8a3355c3de
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-02-16 16:04:49 +0300

    30. ders sonu

--- MVCProjeKamp/Controllers/CategoryController.cs
+++ MVCProjeKamp/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,26 @@ namespace MVCProjeKamp.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+       CategoryManager cm = new CategoryManager(new EFCategoryDal());
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+           // cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
