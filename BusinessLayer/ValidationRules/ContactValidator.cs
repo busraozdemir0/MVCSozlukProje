b3f01d08aa0633bb87698653c90363de1f8fbc43
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-02-19 16:47:49 +0300

    60. ders sonu

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator() 
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail alanını boş geçemezsiniz!.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu alanını boş geçemezsiniz!");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adını boş geçemezsiniz!");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Lütfen en az 3 karakterlik veri girişi yapın!");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakterlik veri girişi yapın!");
+            RuleFor(x => x.Subject).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla veri girişi yapmayın!");
+
+        }
+    }
+}
---
+++ MVCProjeKamp/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKamp.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        ContactManager cm = new ContactManager(new EfContactDal());
+        ContactValidator cv = new ContactValidator();
+        public ActionResult Index()
+        {
+            var contactValues = cm.GetList();
+            return View(contactValues);
+        }
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues=cm.GetByID(id);
+            return View(contactValues);
+        }
+    }
+}
