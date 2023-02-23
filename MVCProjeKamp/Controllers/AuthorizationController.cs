3d27743d76a947910429ced4f29963f70e8caee5
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-02-23 12:59:24 +0300

    100. ders sonu - yetkilendirmeler

---
+++ MVCProjeKamp/Controllers/AuthorizationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKamp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+        AdminManager adminManager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminValues = adminManager.GetList();
+            return View(adminValues);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            adminManager.AdminAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            var adminValue = adminManager.GetByID(id);
+            return View(adminValue);
+        }
+        [HttpPost]
+        public ActionResult EditAdmin(Admin admin)
+        {
+            adminManager.AdminUpdate(admin);
+            return RedirectToAction("Index");
+        }
+    }
+}
