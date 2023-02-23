6721d883bfcbb226297b4238462bb5720b534ede
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-02-23 10:55:20 +0300

    94. ders - chart

---
+++ MVCProjeKamp/Controllers/ChartController.cs
@@ -0,0 +1,47 @@
+﻿using MVCProjeKamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryChart()
+        {   //köprü görevi görecek olan action
+            return Json(BlogList(),JsonRequestBehavior.AllowGet);
+        }
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct=new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName="Yazılım",
+                CategoryCount=8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return ct;
+        }
+    }
+}
